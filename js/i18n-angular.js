d9d061f61bfa9a80b654e0d4669291f42e0fc9d5
Senio Caires <seniocaires@gmail.com>
2015-10-28 21:39:59 -0200

    Get

--- js/i18n-angular.js
+++ js/i18n-angular.js
@@ -3,11 +3,9 @@ var app = angular.module('jsrApp', []);
 app.controller('jsrController', function($scope, $http) {
 
 	var lang = window.navigator.language;
-	alert(lang);
 	$scope.getMessages = function() {
-		$http.get("msg/msg_" + lang + ".json")
+		$http.get("js/msg/msg_" + lang + ".json")
 		.success(function (response) {
-			alert(response);
 			$scope.msg = response;
 		});
 	};
