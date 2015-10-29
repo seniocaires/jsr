b54009cc7902beb93728f486c580b68c60b7fb66
Senio Caires <seniocaires@gmail.com>
2015-10-29 10:19:21 -0200

    Update i18n-angular-311.js

--- js/i18n-angular-311.js
+++ js/i18n-angular-311.js
@@ -4,7 +4,7 @@ app.controller('jsrController', function($scope, $http) {
 
 	var lang = window.navigator.language;
 	$scope.getMessages = function() {
-		$http.get("/jsr/js/msg/msg_311_" + lang + ".json")
+		$http.get("/jsr/js/msg/311/msg_" + lang + ".json")
 		.success(function (response) {
 			$scope.msg = response;
 		});
